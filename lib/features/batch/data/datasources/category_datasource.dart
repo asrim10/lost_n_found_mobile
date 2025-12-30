02cf111aed4f7a66a3be46e11e9c8d3ae0a5ee74
asrim10 <asrimmsuwall@gmail.com>
2025-12-30 21:58:57 +0545

    category_datasource

---
+++ lib/features/batch/data/datasources/category_datasource.dart
@@ -0,0 +1,9 @@
+import 'package:lost_n_found/features/batch/data/models/category_hive_model.dart';
+
+abstract interface class ICatregoryDataSource {
+  Future<List<CategoryHiveModel>> getAllCategory();
+  Future<CategoryHiveModel?> getCategoryById(String categoryId);
+  Future<bool> createCategory(CategoryHiveModel category);
+  Future<bool> updateCategory(CategoryHiveModel category);
+  Future<bool> deleteCategory(String category);
+}
