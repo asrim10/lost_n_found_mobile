078132ca0e54b2dd003e5127fb638e9a86a8a198
asrim10 <asrimmsuwall@gmail.com>
2026-01-08 22:25:21 +0545

    auth_local_datasource modified

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,16 +1,18 @@
 import 'package:lost_n_found/features/auth/data/models/auth_hive_model.dart';
 
 abstract interface class IAuthLocalDatasource {
-  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel> register(AuthHiveModel model);
   Future<AuthHiveModel?> login(String email, String password);
   Future<AuthHiveModel?> getCurrentUser();
   Future<bool> logout();
-  // get email exists
-  Future<bool> isEmailExists(String email);
+  Future<AuthHiveModel?> getUserById(String authId);
+  Future<AuthHiveModel?> getUserByEmail(String email);
+  Future<bool> updateUser(AuthHiveModel model);
+  Future<bool> deleteUser(String authId);
 }
 
 abstract interface class IAuthRemoteDatasource {
-  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel> register(AuthHiveModel model);
   Future<AuthHiveModel?> login(String email, String password);
   Future<AuthHiveModel?> getUserById(String authId);
 }
